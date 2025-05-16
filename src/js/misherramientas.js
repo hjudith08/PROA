df3ab77fa8324ebc39fa162cfae8dd871e5d27c9
hjudith08 <juuudith88@gmail.com>
2025-05-16 03:46:57 +0200

    edusync

--- src/js/misherramientas.js
+++ src/js/misherramientas.js
@@ -27,3 +27,32 @@ function irASlide(indice) {
 document.addEventListener('DOMContentLoaded', () => {
   mostrarSlide(0);
 });
+
+
+const logoUser = document.querySelector('.logo-user');
+  const dropdown = document.getElementById('dropdownUsuario');
+  const popup = document.getElementById('popupCerrarSesion');
+
+  logoUser.addEventListener('click', () => {
+    dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
+  });
+
+  document.addEventListener('click', function (e) {
+    if (!logoUser.contains(e.target) && !dropdown.contains(e.target)) {
+      dropdown.style.display = 'none';
+    }
+  });
+
+  function mostrarPopupCerrarSesion() {
+    popup.style.display = 'flex';
+    dropdown.style.display = 'none';
+  }
+
+  function ocultarPopupCerrarSesion() {
+    popup.style.display = 'none';
+  }
+
+  function cerrarSesion() {
+    // Aquí puedes limpiar cookies, localStorage o redirigir
+    window.location.href = '../../../index.html'; // o la ruta que corresponda
+  }

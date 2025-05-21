c2a48b2654c99975be8ff6186791e6244444d06a
Fabi <fausecher@gmail.com>
2025-05-21 21:42:09 +0200

    Estructura Base del Programa
    Hay comentarios que aclaran mucha cosas, en caso de dudas me avisais al privado y os explico como trabajarla, en proa ya podeis empezar a crear el contenido, lo haceis en un include 'direccionDelInclude' y luego lo añadis, luego hare las correcciónes de forma que por ejemplo, en el inicio general se vea un include segun el rol, hacedlo y yo me encargo de eso ultimo luego, y a organizar todo, hay una seccion para includes y bueno eso

---
+++ src/app/proa/profeAlumno/inicioAsignatura.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROA | Herramienta Educativa</title>
+    <!-- imagen de pestaña -->
+    <link rel="icon" href="imagenes/LogosProaBlancoV3.png" type="image/png">
+    <link rel="stylesheet" href="../../../css/proaCSS/estilosBaseProa.css">
+    <script src="../../../js/proaJS/funcionesBase.js" defer></script>
+</head>
+<!-- Cuerpo de la página web -->
+<body>
+
+<!-- Header de Proa (móvil y ordenador) -->  
+<?php include '../../includes/proaInc/menuProa.inc'; ?>
+<!-- Sidebar de Proa -->
+<?php include '../../includes/proaInc/sidebarProaGeneral.inc'; ?>
+
+<!-- Contenido de la página -->
+<div class="contenido">
+    <!-- Titulo de la seccion-->
+    <h2>Inicio de Asignatura</h2>
+<!--  recorrido de donde te situas -->
+    <div class="migas">
+        <a href="inicioGeneral.php">Inicio General / </a><a href="asignaturas.php">Asignaturas / </a><a href="inicioAsignatura.php">Inicio Asignatura / </a>
+    </div>
+<!-- Contenido de la seccion-->
+    <div class="contenido-interior">
+
+        <!--Rellenar-->
+        
+        <!-- lo haceis con lo siguiente<?php /* include '../../includes/proaInc/proa[pas-alumno-profesor]/[nombreDelArchivo].inc'; */ ?> -->
+
+    </div>  
+</div>
+<!-- Footer de Proa -->
+<?php include '../../includes/proaInc/footerProa.inc'; ?>
+
+</body>
+</html>
---
+++ src/app/proa/profeAlumno/inicioGeneral.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROA | Herramienta Educativa</title>
+    <!-- imagen de pestaña -->
+    <link rel="icon" href="imagenes/LogosProaBlancoV3.png" type="image/png">
+    <link rel="stylesheet" href="css/inicioAlumno.css">
+    <link rel="stylesheet" href="../../../css/proaCSS/estilosBaseProa.css">
+    <script src="../../../js/proaJS/funcionesBase.js" defer></script>
+</head>
+<!-- Cuerpo de la página web -->
+<body>
+
+<!-- Header de Proa (móvil y ordenador) -->  
+<?php include '../../includes/proaInc/menuProa.inc'; ?>
+<!-- Sidebar de Proa -->
+<?php include '../../includes/proaInc/sidebarProaGeneral.inc'; ?>
+
+<!-- Contenido de la página -->
+<div class="contenido">
+    <!-- Titulo de la seccion-->
+    <h2>Inicio General</h2>
+<!--  recorrido de donde te situas -->
+    <div class="migas">
+        <a class="ubicacion-actual" href="#">Inicio General / </a>
+    </div>
+<!-- Contenido de la seccion-->
+    <div class="contenido-interior">
+      
+        <!--Rellenar-->
+        
+        <!-- lo haceis con lo siguiente<?php /* include '../../includes/proaInc/proa[pas-alumno-profesor]/[nombreDelArchivo].inc'; */ ?> -->
+
+    </div>
+</div>
+<!-- Footer de Proa -->
+<?php include '../../includes/proaInc/footerProa.inc'; ?>
+
+</body>
+</html>

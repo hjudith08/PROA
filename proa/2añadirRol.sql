17c5373988febf9c01eb2c77fb132ef967c34a29
Joan Catala Sendra <119068779+JoanCataSend@users.noreply.github.com>
2025-05-24 21:20:26 +0200

    Delete proa directory

--- proa/2añadirRol.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO rol (rol_id, nombre_rol, descripcion) VALUES
-('alumno', 'Alumno', 'Estudiante matriculado'),
-('profesor', 'Profesor', 'Personal docente'),
-('pas', 'PAS', 'Personal de administración y servicios');

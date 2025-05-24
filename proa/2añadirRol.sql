359fcdb21b77b49fb05f319d7171fe8ecf1ad03d
Joan Catala Sendra <119068779+JoanCataSend@users.noreply.github.com>
2025-05-24 21:14:03 +0200

    Add files via upload

---
+++ proa/2añadirRol.sql
@@ -0,0 +1,4 @@
+INSERT INTO rol (rol_id, nombre_rol, descripcion) VALUES
+('alumno', 'Alumno', 'Estudiante matriculado'),
+('profesor', 'Profesor', 'Personal docente'),
+('pas', 'PAS', 'Personal de administración y servicios');

0253fdc66eff57d90c4027387bfb0dc2ef761fd5
Fabi <fausecher@gmail.com>
2025-05-30 03:57:59 +0200

    biienennnn

--- src/app/proa/profeAlumno/tareas.php
+++ src/app/proa/profeAlumno/tareas.php
@@ -9,7 +9,6 @@ if (!$id_asignatura) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,41 +21,39 @@ if (!$id_asignatura) {
     <script src="../../../js/proaJS/funcionesBase.js" defer></script>
 </head>
 <!-- Cuerpo de la página web -->
-
 <body>
 
-    <!-- Header de Proa (móvil y ordenador) -->
-    <?php include '../../includes/proaInc/menuProa.inc'; ?>
-    <!-- Sidebar de Proa -->
-    <?php include '../../includes/proaInc/sidebarProaGeneral.inc'; ?>
+<!-- Header de Proa (móvil y ordenador) -->  
+<?php include '../../includes/proaInc/menuProa.inc'; ?>
+<!-- Sidebar de Proa -->
+<?php include '../../includes/proaInc/sidebarProaGeneral.inc'; ?>
 
-    <!-- Contenido de la página -->
-    <div class="contenido">
-        <!-- Titulo de la seccion-->
-        <h2>Tareas</h2>
-        <!--  recorrido de donde te situas -->
-        <div class="migas">
-            <a href="inicioGeneral.php">Inicio General / </a>
-            <a href="asignaturas.php">Asignaturas / </a>
-            <a href="inicioAsignatura.php">Inicio Asignatura / </a>
-            <a href="#">Tareas /</a>
-        </div>
-        <!-- Contenido de la seccion-->
-        <div class="contenido-interior">
-
-            <?php
-            if ($userdata['rol'] == 'alumno') {
-                include '../../includes/proaInc/proaAlumnos/tareasAlumno.inc';
-            } elseif ($userdata['rol'] == 'profesor') {
-                include '../../includes/proaInc/proaProfesores/tareasProfesor.inc';
-            }
-            ?>
-
-        </div>
+<!-- Contenido de la página -->
+<div class="contenido">
+<!-- Titulo de la seccion-->
+    <h2>Tareas</h2>
+<!--  recorrido de donde te situas -->
+    <div class="migas">
+        <a href="inicioGeneral.php">Inicio General / </a>
+        <a href="asignaturas.php">Asignaturas / </a>
+        <a href="inicioAsignatura.php">Inicio Asignatura / </a>
+        <a href="#">Tareas /</a>
     </div>
-    <!-- Footer de Proa -->
-    <?php include '../../includes/proaInc/footerProa.inc'; ?>
+<!-- Contenido de la seccion-->
+    <div class="contenido-interior">
+        
+        <?php 
+        if($userdata['rol'] == 'alumno'){
+            include '../../includes/proaInc/proaAlumnos/tareasAlumno.inc';
+        }elseif($userdata['rol'] == 'profesor'){
+            include '../../includes/proaInc/proaProfesores/tareasProfesor.inc';
+        }
+        ?>
+      
+    </div>
+</div>
+<!-- Footer de Proa -->
+<?php include '../../includes/proaInc/footerProa.inc'; ?>
 
 </body>
-
 </html>

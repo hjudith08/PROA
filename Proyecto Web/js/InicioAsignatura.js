0fe1e8e252138172fad07cedea8f664734360091
Joan Catala Sendra <119068779+JoanCataSend@users.noreply.github.com>
2025-06-02 20:01:23 +0200

    Delete Proyecto Web directory

--- Proyecto Web/js/InicioAsignatura.js
+++
@@ -1,9 +0,0 @@
-    document.querySelectorAll('.asignaturas li').forEach(item => {
-    item.addEventListener('click', () => {
-        const texto = item.querySelector('.texto-desplegable');
-        if (texto) {
-            texto.style.display = (texto.style.display === 'block') ? 'none' : 'block';
-        }
-    });
-});
-
